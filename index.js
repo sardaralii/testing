0dc16ed28678de7b40c8db29d2a0094af023dee8
sardarali <95859314+sardaralii@users.noreply.github.com>
2023-09-10 17:52:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const http = require('http');
-
-const hostname = '0.0.0.0'; // Listen on all available network interfaces
-const port = 3000;
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  // Handling the root URL '/'
+  if (req.url === '/') {
+    // Read the HTML file
+    const filePath = path.join(__dirname, 'index.html');
+    
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other routes or files here if needed
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server is running on http://${hostname}:${port}`);
+const port = 8080; // Use a different port, e.g., 8080
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
